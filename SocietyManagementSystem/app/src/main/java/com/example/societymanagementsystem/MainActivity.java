9b3f29adedbedcf3868398cf22ac7dded6c7248c
sys-de-chaos <79124741+sys-de-chaos@users.noreply.github.com>
2021-09-19 15:32:21 +1000

    Fully Working Source
    Added custom icon but not working at the moment

--- SocietyManagementSystem/app/src/main/java/com/example/societymanagementsystem/MainActivity.java
+++ SocietyManagementSystem/app/src/main/java/com/example/societymanagementsystem/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.societymanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,28 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CustomWebViewClient client = new CustomWebViewClient(activity:this);
+        CustomWebViewClient client = new CustomWebViewClient(this);
+
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(client);
-        webView.getSetting().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+
         webView.loadUrl("https://www.google.com");
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode) == KeyEvent.KEYCODE_BACK && this.webView.canGoBack(){
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack()){
             this.webView.goBack();
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
-
-
 }
 
 class CustomWebViewClient extends WebViewClient{
     private Activity activity;
-    publiv CustomWebViewClient(Activity activity){
+    public CustomWebViewClient(Activity activity){
         this.activity = activity;
     }
 
